c99e79a76cda70912c81f84208b2eee6f099c1b0
Jacob Shieh <jacob.shieh@gmail.com>
2019-02-24 20:32:31 -0800

    project init

---
+++ src/main/kotlin/com/affjj/pileofshame/PileOfShameApplication.kt
@@ -0,0 +1,11 @@
+package com.affjj.pileofshame
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PileOfShameApplication
+
+fun main(args: Array<String>) {
+	runApplication<PileOfShameApplication>(*args)
+}
