ece28c0cf5d504e76252c3ce23047fa7741135ec
Jacob Shieh <jacob.shieh@gmail.com>
2019-02-26 22:04:20 -0800

    add table name to work with postgres

--- src/main/kotlin/com/affjj/pileofshame/model/User.kt
+++ src/main/kotlin/com/affjj/pileofshame/model/User.kt
@@ -1,8 +1,10 @@
 package com.affjj.pileofshame.model
 
 import javax.persistence.Entity
+import javax.persistence.Table
 
 @Entity
+@Table(name = "users")
 class User(
     val name: String? = null,
     val email: String? = null
